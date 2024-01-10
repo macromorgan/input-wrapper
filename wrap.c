a6e845907ebfb5696de22e26d0edb8e43f99e2d2
Chris Morgan <macromorgan@hotmail.com>
2024-01-10 13:11:33 -0600

    input-wrapper: Add EV_ABS and EV_KEY support
    Finish adding support for EV_ABS and EV_KEY devices so that we can
    use this to create a full controller for userspace.
    
    Signed-off-by: Chris Morgan <macromorgan@hotmail.com>

--- wrap.c
+++ wrap.c
@@ -24,17 +24,23 @@
 
 /* Value comes from kernel define FF_MEMLESS_EFFECTS which is 16 */
 #define MAX_FF_EFFECTS		16
+#define MAX_KEY_DEVS		8
 
 #define ARRAY_SIZE(array)	(sizeof(array) / sizeof(*array))
+#define	TEST_BIT(bit, array)	(array[bit / 8] & (1 << (bit % 8)))
 
 /*
  * The struct that contains the necessary data to manage the virtual
- * input device.
+ * input device. We currently support a single force feedback device,
+ * a single abs device, and multiple key devices.
  */
 struct virtual_device {
 	struct uinput_setup usetup;
+	struct uinput_abs_setup uabssetup[ABS_MAX];
 	int uinput_fd;
 	int ff_fd;
+	int abs_fd;
+	int key_fd[MAX_KEY_DEVS];
 };
 
 struct dev_info {
@@ -43,8 +49,9 @@ struct dev_info {
 
 /*
  * List of all the "devices of interest" that we're looking to
- * capture. As of right now though we are only playing with the
- * pwm-vibrator.
+ * capture. Only the last ff device, last abs device, and first
+ * 10 key devices that match the names below will be used by the
+ * driver.
  */
 static struct dev_info input_devs[] = {
 	{ .name = "pwm-vibrator" },
@@ -55,6 +62,83 @@ static struct dev_info input_devs[] = {
 };
 
 /**
+ * enumerate_abs_device() - Identify ABS axes and features
+ * @v_dev: pointer to virtual_device struct
+ *
+ * Enumerate ABS axes and add them to the uinput virtual device.
+ * Return 0 on success or negative on error.
+ */
+int enumerate_abs_device(struct virtual_device *v_dev)
+{
+	uint8_t abs_b[ABS_MAX/8 + 1];
+	int ret = 0;
+
+	if (v_dev->abs_fd <= 0)
+		return 0;
+
+	ret = ioctl(v_dev->abs_fd,
+		    EVIOCGBIT(EV_ABS, sizeof(abs_b)), abs_b);
+	if (ret < 0) {
+		printf("Unable to enumerate ABS device: %d\n", ret);
+		return -ENODEV;
+	}
+
+	for (int i = 0; i < ABS_MAX; i++) {
+		if (TEST_BIT(i, abs_b)) {
+			ret = ioctl(v_dev->abs_fd, EVIOCGABS(i),
+				    &v_dev->uabssetup[i].absinfo);
+			if (ret)
+				continue;
+
+			ret = ioctl(v_dev->uinput_fd,
+				    UI_SET_ABSBIT, i);
+			if (ret)
+				continue;
+			v_dev->uabssetup[i].code = i;
+			ret = ioctl(v_dev->uinput_fd, UI_ABS_SETUP,
+				    &v_dev->uabssetup[i]);
+			if (ret)
+				printf("Unable to set abs axis %d\n",
+				       i);
+		}
+	}
+	return 0;
+}
+
+/**
+ * enumerate_key_devices() - Identify supported keys
+ * @v_dev: pointer to virtual_device struct
+ *
+ * Enumerate keys and add them to the uinput virtual device. Return
+ * number of keys identified.
+ */
+int enumerate_key_devices(struct virtual_device *v_dev)
+{
+	uint8_t key_b[KEY_MAX/8 + 1];
+	int dev_count = 0;
+	int keys = 0;
+
+	for (int dev_num = 0; dev_num < MAX_KEY_DEVS; dev_num++) {
+		if (v_dev->key_fd[dev_num] > 0)
+			dev_count += 1;
+	}
+
+	for (int k = 0; k < dev_count; k++) {
+		ioctl(v_dev->key_fd[k],
+		      EVIOCGBIT(EV_KEY, sizeof(key_b)), key_b);
+		for (int i = 0; i < KEY_MAX; i++) {
+			if (TEST_BIT(i, key_b)) {
+				ioctl(v_dev->uinput_fd,
+				      UI_SET_KEYBIT, i);
+				keys += 1;
+			}
+		}
+	}
+
+	return keys;
+}
+
+/**
  * iterate_input_devices() - Identify input devices to be monitored
  * @v_dev: pointer to virtual_device struct
  *
@@ -63,6 +147,9 @@ static struct dev_info input_devs[] = {
  * FF devices are closed as read-only and reopened as write-only, since
  * we need to write to them but not necessarily read them. Return is
  * total number of devices found.
+ *
+ * TODO: Cleanup how we open and add file descriptors to the main
+ * struct.
  */
 int iterate_input_devices(struct virtual_device *v_dev)
 {
@@ -70,6 +157,7 @@ int iterate_input_devices(struct virtual_device *v_dev)
 	char name[256];
 	int fd, ret;
 	int count = 0;
+	int key_devs = 0;
 	unsigned long evbit = 0;
 
 	for (int i = 0; i < 256; i++) {
@@ -80,10 +168,33 @@ int iterate_input_devices(struct virtual_device *v_dev)
 		ret = ioctl(fd, EVIOCGNAME(256), name);
 		ret = ioctl(fd, EVIOCGBIT(0, sizeof(evbit)), &evbit);
 		for (int i = 0; i < ARRAY_SIZE(input_devs); i++) {
-			if (!strcmp(name, input_devs[i].name) &&
-					 (evbit & (1 << EV_FF))) {
-				v_dev->ff_fd = open(fd_dev, O_WRONLY);
-				count += 1;
+			if (!strcmp(name, input_devs[i].name)) {
+				if (evbit & (1 << EV_FF)) {
+					v_dev->ff_fd = open(fd_dev,
+							    O_WRONLY);
+					printf("Found EV_FF: %s\n",
+					       fd_dev);
+					count += 1;
+				}
+				if (evbit & (1 << EV_ABS)) {
+					v_dev->abs_fd = open(fd_dev,
+							     O_RDONLY |
+							     O_NONBLOCK);
+					printf("Found EV_ABS: %s\n",
+					       fd_dev);
+					count += 1;
+				}
+				if (evbit & (1 << EV_KEY)) {
+					if (key_devs >= MAX_KEY_DEVS)
+						continue;
+					v_dev->key_fd[key_devs] = open(fd_dev,
+								       O_RDONLY |
+								       O_NONBLOCK);
+					printf("Found EV_KEY: %s\n",
+					       fd_dev);
+					count += 1;
+					key_devs += 1;
+				}
 			}
 		}
 		close(fd);
@@ -100,6 +211,8 @@ int iterate_input_devices(struct virtual_device *v_dev)
  * monitored input devices into a single one that applications are
  * more capable of dealing with. Return 0 on success, negative on
  * error.
+ *
+ * TODO: enumerate force feedback capabilites instead of hard-coding.
  */
 int create_uinput_device(struct virtual_device *v_dev)
 {
@@ -109,6 +222,26 @@ int create_uinput_device(struct virtual_device *v_dev)
 					       O_DSYNC | O_RSYNC);
 	if (v_dev->uinput_fd == -1)
 		return -ENODEV;
+
+	if (v_dev->abs_fd > 0) {
+		ret = ioctl(v_dev->uinput_fd, UI_SET_EVBIT, EV_ABS);
+		if (ret)
+			return ret;
+		ret = enumerate_abs_device(v_dev);
+		if (ret)
+			return ret;
+	}
+
+	if (v_dev->key_fd[0] > 0) {
+		ret = ioctl(v_dev->uinput_fd, UI_SET_EVBIT, EV_KEY);
+		if (ret)
+			return ret;
+		ret = enumerate_key_devices(v_dev);
+		if (!(ret > 0)) {
+			printf("No keys found\n");
+			return -ENODEV;
+		}
+	}
 
 	if (v_dev->ff_fd > 0) {
 		ret = ioctl(v_dev->uinput_fd, UI_SET_EVBIT, EV_FF);
@@ -178,7 +311,6 @@ int handle_uinput_ff_upload(struct virtual_device *v_dev,
 
 	return 0;
 }
-
 
 /**
  * handle_uinput_ff_erase() - Capture and respond to ff_erase
@@ -273,23 +405,21 @@ int set_ff_effect_status(struct virtual_device *v_dev, int effect,
 
 	return 0;
 }
+
 /**
  * handle_ff_events() - Respond to ff_events
  *
  * @v_dev: main virtual device struct
  * @ev: input_event initiating ff upload
- * @fd_in: file descriptor responsible for ff_event
  *
  * Dispatch an ff event to the correct ff handler. Return value is 0
- * for success, negative for error.
+ * for success, negative for error. For some reason it was insufficient
+ * to simply forward the input_event, we had to create a new one.
  */
 int handle_ff_events(struct virtual_device *v_dev,
 		     struct input_event ev)
 {
 	int ret = 0;
-
-	if (ev.type != EV_FF)
-		return 0;
 
 	if (ev.code == FF_GAIN)
 		return set_ff_gain(v_dev, ev.value);
@@ -297,8 +427,9 @@ int handle_ff_events(struct virtual_device *v_dev,
 	if (ev.code <= FF_GAIN)
 		return set_ff_effect_status(v_dev, ev.code, ev.value);
 
-	return 0;
+	return ret;
 }
+
 /**
  * parse_ev_incoming() - Process incoming event and hand off to correct
  * helper function.
@@ -306,7 +437,7 @@ int handle_ff_events(struct virtual_device *v_dev,
  * @v_dev: main virtual device struct
  * @fd_in: file descriptor responsible for event
  *
- * Process an EPOLLIN queue item and hand off necessary data to correct
+ * Process an EPOLLIN request and hand off necessary data to correct
  * function. Return value is 0 for success, negative for error.
  */
 void parse_ev_incoming(struct virtual_device *v_dev, int fd_in)
@@ -316,14 +447,14 @@ void parse_ev_incoming(struct virtual_device *v_dev, int fd_in)
 
 	len = read(fd_in, &ev, sizeof(ev));
 	if (len != -1) {
-		printf("len: %d\n", len);
-		printf("Type %d Code %d Value %d Time %lu source %d\n",
-			ev.type,
-			ev.code,
-			ev.value,
-			ev.time.tv_sec,
-			fd_in);
 		switch (ev.type) {
+		case EV_SYN:
+		case EV_ABS:
+		case EV_KEY:
+			if (v_dev->uinput_fd != fd_in)
+				write(v_dev->uinput_fd,
+				      &ev, sizeof(ev));
+			break;
 		case EV_UINPUT:
 			if (ev.code == UI_FF_UPLOAD) {
 				handle_uinput_ff_upload(v_dev, ev);
@@ -335,23 +466,80 @@ void parse_ev_incoming(struct virtual_device *v_dev, int fd_in)
 			printf("UINPUT ev %d not handled\n", ev.code);
 			break;
 		case EV_FF:
-			handle_ff_events(v_dev, ev);
+			if (v_dev->uinput_fd == fd_in)
+				handle_ff_events(v_dev, ev);
 			break;
 		default:
-			printf("EV type %d EV code %d not handled\n", ev.type, ev.code);
+			/* Catch for events we don't support yet */
+			printf("EV type %d EV code %d not handled\n",
+			       ev.type, ev.code);
 		}
 
 	} else {
-		printf("read failed descriptor %d, errno %d\n", fd_in, errno);
+		printf("read failed descriptor %d, errno %d\n",
+		       fd_in, errno);
 	}
+}
+
+/**
+ * define_epoll_fds() - Add all required file descriptors to epoll to
+ * be monitored.
+ *
+ * @v_dev: main virtual device struct
+ * @ep_fd: epoll file descriptor
+ *
+ * Iterate through all file descriptors to monitor and add those which
+ * need to be monitored by epoll. At a minimum we need to monitor the
+ * uinput device for force feedback effects and at least 1 of either
+ * an ABS device or 1 or more key devices.
+ */
+int define_epoll_fds(struct virtual_device *v_dev, int ep_fd)
+{
+	struct epoll_event event;
+	int ret = 0;
+
+	event.events = EPOLLIN;
+	event.data.fd = v_dev->uinput_fd;
+	ret = epoll_ctl(ep_fd, EPOLL_CTL_ADD, v_dev->uinput_fd,
+			&event);
+	if (ret == -1) {
+		printf("Cannot monitor uinput device\n");
+		return -1;
+	}
+
+	if (v_dev->abs_fd > 0) {
+		event.events = EPOLLIN;
+		event.data.fd = v_dev->abs_fd;
+		ret = epoll_ctl(ep_fd, EPOLL_CTL_ADD, v_dev->abs_fd,
+				&event);
+		if (ret == -1) {
+			printf("Cannot monitor abs device\n");
+			return -1;
+		}
+	}
+
+	for (int i = 0; i < MAX_KEY_DEVS; i++) {
+		if (!(v_dev->key_fd[i] > 0))
+			continue;
+		event.events = EPOLLIN;
+		event.data.fd = v_dev->key_fd[i];
+		ret = epoll_ctl(ep_fd, EPOLL_CTL_ADD, v_dev->key_fd[i],
+				&event);
+		if (ret == -1) {
+			printf("Cannot monitor key device %d\n", i);
+			return -1;
+		}
+	}
+
+	return 0;
 }
 
 int main(void)
 {
-	struct epoll_event uevent, fevent, event_queue[MAX_EVENTS];
+	struct epoll_event event_queue[MAX_EVENTS];
 	struct virtual_device *v_dev;
 	int ep_fd;
-	int ret;
+	int ret = 0;
 
 	v_dev = malloc(sizeof(v_dev));
 
@@ -368,7 +556,7 @@ int main(void)
 
 	ret = create_uinput_device(v_dev);
 	if (ret) {
-		printf("Unable to create uinput device\n");
+		printf("Unable to create uinput device: %d\n", ret);
 		return -ENODEV;
 	}
 
@@ -378,13 +566,9 @@ int main(void)
 		return -1;
 	}
 
-	uevent.events = EPOLLIN;
-	uevent.data.fd = v_dev->uinput_fd;
-	ret = epoll_ctl(ep_fd, EPOLL_CTL_ADD, v_dev->uinput_fd, &uevent);
-	if (ret == -1) {
-		printf("Cannot monitor uinput device.\n");
-		return -1;
-	}
+	ret = define_epoll_fds(v_dev, ep_fd);
+	if (ret)
+		return ret;
 
 	while (1) {
 		int n, i;
@@ -392,7 +576,8 @@ int main(void)
 		n = epoll_wait(ep_fd, event_queue, 1, -1);
 		for (i = 0; i < n; i++) {
 			if (event_queue[i].events & EPOLLIN)
-				parse_ev_incoming(v_dev, event_queue[i].data.fd);
+				parse_ev_incoming(v_dev,
+						  event_queue[i].data.fd);
 			else {
 				printf("epoll error, type %lu\n",
 				       event_queue[i].events);
